958e233c1c95eeedeae8db6d289b2c5fe774c9be
Guillaume Grossetie <ggrossetie@gmail.com>
2016-10-09 13:55:35 +0200

    Debug on appveyor

--- index.js
+++ index.js
@@ -14,10 +14,16 @@ OpalCompiler.prototype.compile = function (require, outputFile, includes) {
   var builder = Builder.$new();
   var stdlibPath;
   // hierarchical structure (npm < 3.x) 
-  // do not use path.resolve or path.join because this is not working as expected on Windows
-  var stdlibHierarchicalPath = 'node_modules/opal-compiler/src/stdlib';
+  var stdlibHierarchicalPath = path.resolve(__dirname, 'node_modules', 'opal-compiler', 'src', 'stdlib');
+  var stdlibRelativePath = 'node_modules/opal-compiler/src/stdlib';
   // flat structure (npm >= 3.x)
   var stdlibFlatPath = path.resolve(__dirname, '..', 'opal-compiler', 'src', 'stdlib');
+  log.debug('__dirname: ' + __dirname + ', exits? ' + fs.existsSync(__dirname));
+  log.debug('path.resolve(__dirname): ' + path.resolve(__dirname) + ', exits? ' + fs.existsSync(path.resolve(__dirname)));
+  log.debug('path.resolve(__dirname, \'..\'): ' + path.resolve(__dirname, '..') + ', exits? ' + fs.existsSync(path.resolve(__dirname, '..')));
+  log.debug('stdlibFlatPath: ' + stdlibFlatPath + ', exits? ' + fs.existsSync(stdlibFlatPath));
+  log.debug('stdlibHierarchicalPath: ' + stdlibHierarchicalPath + ', exits? ' + fs.existsSync(stdlibHierarchicalPath));
+  log.debug('stdlibRelativePath: ' + stdlibRelativePath + ', exits? ' + fs.existsSync(stdlibRelativePath));
   if (fs.existsSync(stdlibHierarchicalPath)) {
     stdlibPath = stdlibHierarchicalPath;
   } else if (fs.existsSync(stdlibFlatPath)) {
